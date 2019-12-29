6264cc4210ce921c9c74939c46bd25faf843f91f
crysmart <425331631@qq.com>
2019-12-30 01:07:52 +0800

    全传。。

---
+++ filesync/src/main/java/com/filesync/FilesyncApplication.java
@@ -0,0 +1,13 @@
+package com.filesync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilesyncApplication.class, args);
+    }
+
+}
---
+++ filesync/src/test/java/com/filesync/FilesyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filesync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

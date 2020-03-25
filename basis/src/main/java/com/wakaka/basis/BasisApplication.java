01f3fbd70397a7c5ef8b52c7be85fd77c6a0fe9a
crysmart <425331631@qq.com>
2020-03-25 22:57:00 +0800

    添加冒泡排序

---
+++ basis/src/main/java/com/wakaka/basis/BasisApplication.java
@@ -0,0 +1,16 @@
+package com.wakaka.basis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * java基础
+ */
+@SpringBootApplication
+public class BasisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasisApplication.class, args);
+    }
+
+}
---
+++ basis/src/main/java/com/wakaka/basis/array/Arrays.java
@@ -0,0 +1,25 @@
+package com.wakaka.basis.array;
+
+/**
+ * 数组相关
+ *
+ * @author Wang.hm
+ * @date Created in 22:23 2020/3/25
+ */
+public class Arrays {
+
+    /** 冒泡排序:数值交换 */
+    public static int[] BubbleSort(int[] ints) {
+        for (int j = 0; j < ints.length; j++) {
+            for (int i = 0; i < ints.length - 1; i++) {
+                if (ints[i] > ints[i + 1]) {
+                    int temp = ints[i];
+                    ints[i] = ints[i + 1];
+                    ints[i + 1] = temp;
+                }
+            }
+        }
+        return ints;
+    }
+
+}

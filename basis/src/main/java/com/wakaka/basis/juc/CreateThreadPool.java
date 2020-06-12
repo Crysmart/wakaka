bae9db580a0b2b928f12b3680a94f2350a90c5f0
crysmart <425331631@qq.com>
2020-06-12 18:01:30 +0800

    新增线程池创建

--- basis/src/main/java/com/wakaka/basis/juc/CreateThreadPool.java
+++ basis/src/main/java/com/wakaka/basis/juc/CreateThreadPool.java
@@ -1,17 +1,66 @@
 package com.wakaka.basis.juc;
 
+import java.time.Instant;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 创建线程池
  * @author Crysmart
  */
 public class CreateThreadPool {
+    private static final int CORE_POOL_SIZE = 5;
+    private static final int MAX_POOL_SIZE = 10;
+    private static final int QUEUE_CAPACITY = 100;
+    private static final Long KEEP_ALIVE_TIME = 1L;
+
+    public static void main(String[] args) {
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                CORE_POOL_SIZE,
+                MAX_POOL_SIZE,
+                KEEP_ALIVE_TIME,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(QUEUE_CAPACITY),
+                new NameThreadFactory("test")
+        );
+
+        for (int i = 0; i < 10; i++) {
+            executor.execute(() -> {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("CurrentThread name:" + Thread.currentThread().getName() + "date：" + Instant.now());
+            });
+        }
+        //终止线程池
+        executor.shutdown();
+        try {
+            executor.awaitTermination(5, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Finished all threads");
+    }
 }
 
 class NameThreadFactory implements ThreadFactory{
+
+    private final AtomicInteger nextId = new AtomicInteger(1);
+    private final String namePrefix;
+
+
+    public NameThreadFactory(String namePrefix) {
+        this.namePrefix = namePrefix;
+    }
+
     @Override
-    public Thread newThread(Runnable r) {
-        return null;
+    public Thread newThread(Runnable task) {
+        String threadName = namePrefix + " [#" + nextId.getAndIncrement() + "]";
+        return new Thread(null,task,threadName);
     }
 }

28ac94a755f73c60de902c04a4b51c11d9c204ad
crysmart <425331631@qq.com>
2020-09-22 20:34:19 +0800

    create project springcloud-alibaba,todo...

---
+++ framework/springcloud-alibaba/server-nacos/src/main/java/com/wakaka/springcloud/alibaba/NacosServerApplication.java
@@ -0,0 +1,15 @@
+package com.wakaka.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Crysmart
+ * @date 2020/9/22 19:26
+ */
+@SpringBootApplication
+public class NacosServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosServerApplication.class,args);
+    }
+}
---
+++ framework/springcloud-alibaba/server-nacos/src/main/java/com/wakaka/springcloud/alibaba/controller/ConfigController.java
@@ -0,0 +1,24 @@
+package com.wakaka.springcloud.alibaba.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Crysmart
+ * @date 2020/9/22 19:49
+ */
+@RestController
+@RequestMapping("/config")
+@RefreshScope
+public class ConfigController {
+
+    @Value("${useLocalCache:false}")
+    private boolean useLocalCache;
+
+    @RequestMapping("/get")
+    public boolean get() {
+        return useLocalCache;
+    }
+}

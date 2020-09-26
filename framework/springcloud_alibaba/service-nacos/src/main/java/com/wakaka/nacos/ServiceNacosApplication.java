483bcfdc650008d8baa8113c292858141a824c4c
crysmart <425331631@qq.com>
2020-09-26 17:47:30 +0800

    pull update

---
+++ framework/springcloud_alibaba/service-nacos/src/main/java/com/wakaka/nacos/ServiceNacosApplication.java
@@ -0,0 +1,11 @@
+package com.wakaka.nacos;
+
+/**
+ * @author Crysmart
+ * @date 2020/9/12 23:32
+ */
+public class ServiceNacosApplication {
+    public static void main(String[] args) {
+
+    }
+}

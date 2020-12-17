ff68979799678e61289419d84375a78433ed107a
crysmart <425331631@qq.com>
2020-12-17 15:05:09 +0800

    framework模块修改

---
+++ framework/auth/src/main/java/com/wakaka/auth/AuthApplication.java
@@ -0,0 +1,16 @@
+package com.wakaka.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Crysmart
+ * @date 2020/12/17 14:37
+ */
+
+@SpringBootApplication
+public class AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class,args);
+    }
+}

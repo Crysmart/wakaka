5359f743eb9bf3daa2cbc8c6a65ae035f10bba86
crysmart <425331631@qq.com>
2021-01-05 18:08:57 +0800

    新建guava工具模块

---
+++ util/guava/src/main/java/com/wakaka/App.java
@@ -0,0 +1,13 @@
+package com.wakaka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

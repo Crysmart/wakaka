967907fbad36a3bb1edf0af2da8eba381a8e2599
crysmart <425331631@qq.com>
2021-03-04 15:54:46 +0800

    添加事务MQ类型

--- framework/rocketmq/src/main/java/com/wakaka/mq/transaction/tranConsumer.java
+++ framework/rocketmq/src/main/java/com/wakaka/mq/transaction/tranConsumer.java
@@ -1,8 +1,29 @@
 package com.wakaka.mq.transaction;
 
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.*;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
 /**
  * @author Crysmart
  * @date 2021/2/26 16:24
  */
 public class tranConsumer {
+    public static void main(String[] args) throws Exception {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("testgroup");
+        consumer.setNamesrvAddr("192.168.6.193:9876");
+        consumer.subscribe("toppp","taggg");
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                msgs.forEach(System.out::println);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        consumer.start();
+
+    }
 }
--- framework/rocketmq/src/main/java/com/wakaka/mq/transaction/tranProducer.java
+++ framework/rocketmq/src/main/java/com/wakaka/mq/transaction/tranProducer.java
@@ -1,8 +1,42 @@
 package com.wakaka.mq.transaction;
 
+import org.apache.rocketmq.client.producer.LocalTransactionState;
+import org.apache.rocketmq.client.producer.TransactionListener;
+import org.apache.rocketmq.client.producer.TransactionMQProducer;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.nio.charset.StandardCharsets;
+
 /**
  * @author Crysmart
  * @date 2021/2/26 16:24
  */
 public class tranProducer {
+    private static int count;
+    public static void main(String[] args) throws Exception {
+        TransactionMQProducer producer = new TransactionMQProducer("testgroup");
+        producer.setNamesrvAddr("192.168.6.193:9876");
+        producer.setTransactionListener(new TransactionListener() {
+            @Override
+            public LocalTransactionState executeLocalTransaction(Message msg, Object arg) {
+                System.out.println(msg);
+                return LocalTransactionState.COMMIT_MESSAGE;
+            }
+
+            @Override
+            public LocalTransactionState checkLocalTransaction(MessageExt msg) {
+                count = count++;
+                System.out.println(count);
+                System.err.println(msg);
+                return LocalTransactionState.UNKNOW;
+            }
+        });
+        producer.start();
+        for (int i = 0; i < 10; i++) {
+            Message message = new Message("test", "testtags", "666".getBytes(StandardCharsets.UTF_8));
+            producer.sendMessageInTransaction(message,null);
+        }
+        producer.shutdown();
+    }
 }

7e50a8c6d0164a8a2dc69f0c79f2832601378a82
crysmart <425331631@qq.com>
2021-04-07 16:39:31 +0800

    添加dubbo2

---
+++ framework/dubbo2/src/main/java/com/wakaka/dubbo2/Dubbo2Application.java
@@ -0,0 +1,13 @@
+package com.wakaka.dubbo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dubbo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dubbo2Application.class, args);
+	}
+
+}
---
+++ framework/dubbo2/src/test/java/com/wakaka/dubbo2/Dubbo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wakaka.dubbo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dubbo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

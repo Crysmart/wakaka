d3012fbc4956e53270f4b7f8815c114ce43a75aa
crysmart <425331631@qq.com>
2021-07-03 12:05:42 +0800

    新增spring-boot-admin健康检测

---
+++ framework/spring-admin/admin-client/src/main/java/com/wakaka/adminclient/AdminClientApplication.java
@@ -0,0 +1,15 @@
+package com.wakaka.adminclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AdminClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminClientApplication.class, args);
+	}
+
+}

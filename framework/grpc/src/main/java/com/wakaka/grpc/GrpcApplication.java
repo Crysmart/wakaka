189b44672e4323666b14c9bdeb96ff2c31310f5b
crysmart <425331631@qq.com>
2021-07-27 15:24:55 +0800

    添加prometheus采集注册

--- framework/grpc/src/main/java/com/wakaka/grpc/GrpcApplication.java
+++ framework/grpc/src/main/java/com/wakaka/grpc/GrpcApplication.java
@@ -1,6 +1,5 @@
 package com.wakaka.grpc;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
@@ -8,7 +7,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class GrpcApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(GrpcApplication.class, args);
+		//SpringApplication.run(GrpcApplication.class, args);
+		int a = 1000&1100;
+		String s = Integer.toBinaryString(a);
+		System.out.println(s);
+		int i = Integer.parseInt("1001000");
+		System.out.println(i);
 	}
 
 }
--- framework/spring-admin/admin-client/src/main/java/com/wakaka/adminclient/AdminClientApplication.java
+++ framework/spring-admin/admin-client/src/main/java/com/wakaka/adminclient/AdminClientApplication.java
@@ -1,8 +1,12 @@
 package com.wakaka.adminclient;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +14,16 @@ public class AdminClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminClientApplication.class, args);
+	}
+
+	/**
+	 * prometheus 采集注册
+	 * @param applicationName
+	 * @return
+	 */
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName){
+		return registry -> registry.config().commonTags("application", applicationName);
 	}
 
 }

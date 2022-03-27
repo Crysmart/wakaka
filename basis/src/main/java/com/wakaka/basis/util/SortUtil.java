61c009690ffbaf647ccbe0ee1b508c8773b73369
425331631@qq.com <whm425331631-+>
2022-03-27 22:19:45 +0800

    add sortUtil bubbling and redis module

---
+++ basis/src/main/java/com/wakaka/basis/util/SortUtil.java
@@ -0,0 +1,31 @@
+package com.wakaka.basis.util;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class SortUtil {
+    public static void main(String[] args) {
+        int len = 10;
+        int maxInt = 1000;
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = new Random().nextInt(maxInt);
+        }
+        sortBubbling(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    /** 冒泡排序 **/
+    public static void sortBubbling(int[] arr){
+        int arrLen = arr.length;
+        for (int i = 0; i < arrLen; i++) {
+            for (int j = 0; j < arrLen -i -1; j++) {
+                if (arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ framework/redis/src/main/java/com/cry/RedisApplication.java
@@ -0,0 +1,24 @@
+package com.cry;
+
+import com.cry.controller.JobController;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+/**
+ * @author Crysmart
+ * @date 2020/7/21 14:46
+ */
+@SpringBootApplication
+public class RedisApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(SbApplication.class,args);
+    }
+
+}

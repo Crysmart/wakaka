eae721a056deebec6ef859dd006a6515f34e5cda
crysmart <425331631@qq.com>
2022-12-28 22:20:37 +0800

    1

--- framework/groovy/src/main/java/com/wakaka/groovy/GroovyApplication.java
+++ framework/groovy/src/main/java/com/wakaka/groovy/GroovyApplication.java
@@ -2,22 +2,17 @@ package com.wakaka.groovy;
 
 
 import groovy.lang.GroovyShell;
-import groovy.lang.Script;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.Reader;
 import java.util.HashMap;
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class GroovyApplication {
 
+    private static final Logger logger = Logger.getAnonymousLogger();
     public static void main(String[] args) {
+
         HashMap<String, String> hashMap = new HashMap<>();
         hashMap.put("a","b");
         hashMap.put("c","d");
@@ -26,12 +21,12 @@ public class GroovyApplication {
         Object helloo = new GroovyShell().parse("String helloo(HashMap<String,String> map){\n" +
                         "        println map.get(\"a\")\n" +
                         "        println(map)\n" +
-                        "        return map.get(\"c\");\n" +
+                        "        return map.get(\"c\")\n" +
                         "    }")
                 .invokeMethod("helloo", hashMap);
-        System.out.println((String) helloo);
+        logger.info((String) helloo);
 
-        System.out.println(new GroovyShell().evaluate(""));
+        new GroovyShell().evaluate("println(\"hello world\")");
     }
 
 
